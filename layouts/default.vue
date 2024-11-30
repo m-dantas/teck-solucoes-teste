dbdbc8a3bd643f7aabb0aeeadafdae50fd1238f2
m-dantas <mauricio.dantascp@gmail.com>
2024-11-30 14:21:52 -0300

    feat: add component Alert no layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <slot />
+    <Alert></Alert>
   </div>
 </template>
 
 <script lang="ts">
+import Alert from '~/components/Alert.vue';
 
+export default defineComponent({
+  name: 'default',
+  components: {
+    Alert
+  }
+})
 </script>
 
 <style>
