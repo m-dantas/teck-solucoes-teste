ac7303615413c11ad898aaafd8ba616cecb9cda9
m-dantas <mauricio.dantascp@gmail.com>
2024-11-30 13:23:53 -0300

    feat: add novas validacoes para serem usadas no projeto

--- utils/cpf.ts
+++ utils/cpf.ts
@@ -1,21 +1,26 @@
-export default (strCPF: string) => {
-  const cpfWithOutDot = strCPF.replaceAll('.', '').replace('-', '')
-  let Soma;
-  let Resto;
-  Soma = 0;
-  if (cpfWithOutDot == "00000000000") return false;
-
-  for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(cpfWithOutDot.substring(i - 1, i)) * (11 - i);
-  Resto = (Soma * 10) % 11;
-
-  if ((Resto == 10) || (Resto == 11)) Resto = 0;
-  if (Resto != parseInt(cpfWithOutDot.substring(9, 10))) return false;
-
-  Soma = 0;
-  for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(cpfWithOutDot.substring(i - 1, i)) * (12 - i);
-  Resto = (Soma * 10) % 11;
-
-  if ((Resto == 10) || (Resto == 11)) Resto = 0;
-  if (Resto != parseInt(cpfWithOutDot.substring(10, 11))) return false;
-  return true;
+export default {
+  validation: (strCPF: string): boolean => {
+    const cpfWithOutDot = strCPF.replaceAll('.', '').replace('-', '')
+    let Soma;
+    let Resto;
+    Soma = 0;
+    if (cpfWithOutDot == "00000000000") return false;
+  
+    for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(cpfWithOutDot.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+  
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(cpfWithOutDot.substring(9, 10))) return false;
+  
+    Soma = 0;
+    for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(cpfWithOutDot.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+  
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(cpfWithOutDot.substring(10, 11))) return false;
+    return true;
+  },
+  mask: (cpf: string): string => {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  }
 }
