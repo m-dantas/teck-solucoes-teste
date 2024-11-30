c2f8bac44e31a6e9837347c270fd10e0f6a0be6f
m-dantas <mauricio.dantascp@gmail.com>
2024-11-30 13:24:41 -0300

    feat: add pagina de novo usuario

---
+++ pages/user/register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="p-new-user container">
+    <Header title="New user">      
+      <Button label="Back to list" @click="$router.go(-1)" />
+    </Header>
+    <FormUser @submit:forms="createNewRegister" />
+  </div>
+</template>
+
+<script lang="ts">
+import { mapActions } from 'pinia';
+import FormUser from '~/components/forms/FormUser.vue';
+
+export default defineComponent({
+  name: 'PageRegister',
+  components: {
+    FormUser
+  },
+  setup() {
+    useHead({
+      title: 'User - register'
+    })
+  },
+  methods: {
+    ...mapActions(useUserStore, ['createNewRegister']),
+  }
+})
+</script>
+
+<style>
+
+</style>
