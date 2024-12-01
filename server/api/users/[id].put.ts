20db3fc6eb27877da4740fa1e3efa7e4c39129e4
m-dantas <mauricio.dantascp@gmail.com>
2024-11-30 22:32:23 -0300

    feat: add validacao caso nao encontre algum item

--- server/api/users/[id].put.ts
+++ server/api/users/[id].put.ts
@@ -3,6 +3,10 @@ export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id')
   const body = await readBody(event)
   const index = sharedUsers.findIndex(user => user.id === id)
-  sharedUsers[index] = { id, ...body }
-  return { success: true, message: 'Usuário atualizado com sucesso', data: sharedUsers[index] }
+  if (index !== -1) {    
+    sharedUsers[index] = { id, ...body }
+    return { success: true, message: 'Usuário atualizado com sucesso', data: sharedUsers[index] }
+  }
+  setResponseStatus(event, 404)
+  return { success: false, message: 'Usuário não encontrado', data: {}}
 })
